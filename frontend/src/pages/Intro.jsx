2d631ee3162e848d56335d002b4489c5e98313c9
Sukhdarshan Singh <sukhi@Sukhdarshans-MacBook-Pro.local>
2026-01-24 15:37:09 -0500

    Added a Navbar a Home page, and an intro page

---
+++ frontend/src/pages/Intro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; // Optional in newer React versions (v17+)
+
+const Intro = () => {
+  // Component logic and state management (using hooks like useState, useEffect) go here
+  return (
+    <div className="Home-container">
+      <p>Bla Bla Bla Something something. Bla Bla</p>
+    </div>
+  );
+};
+
+export default Intro;
