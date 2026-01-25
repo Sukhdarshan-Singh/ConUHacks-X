7a8876d5c0ae22a33098520237d1b2c213c29d42
Cloudedxyekay <114249629+Cloudedxyekay@users.noreply.github.com>
2026-01-25 02:48:39 -0500

    Revert "Merge branch 'main' into backend-api"
    This reverts commit a5c38281bbb04dfd3d06e19ac20dfb9bb5f7bb7a, reversing
    changes made to 46bee084ea5581e23a64a8e6d1c5554d566b2ae6.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,5 @@
-import { useState } from 'react'
-import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
-import Computer from './pages/Computer'; 
-import Navbar from './component/Navbar';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import React from "react";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 
 import Home from "./app/routes/home";
 import Intro from "./app/routes/intro";
@@ -14,21 +8,9 @@ import Chat from "./Chat";
 
 function Layout() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element = {<Home />}/>
-        <Route path="/computer" element = {<Computer/>}/>
-        <Route path="/intro" element = {<Intro/>}/>
-        <Route path = "/game" element = {<Game/>}/>
-        <Route path="*" element = {<h2>404 Not Found</h2>} />
-      </Routes>
-      <Navbar />
-
-      <div className="min-h-screen">
-        {/* put shared UI here if you want (header/sidebar/etc) */}
-         <Outlet />
-      </div>
-    </>
+    <div className="min-h-screen">
+      <Outlet />
+    </div>
   );
 }
 
--- frontend/src/pages/Intro.jsx
+++ frontend/src/pages/Intro.jsx
@@ -1,17 +1,10 @@
 import React from 'react'; // Optional in newer React versions (v17+)
-import {Link} from 'react-router-dom'; 
-
 
 const Intro = () => {
   // Component logic and state management (using hooks like useState, useEffect) go here
   return (
-    <div className="Intro-container">
-      <div >
-        <p>Bla Bla Bla Something something. Bla Bla</p>
-      </div>
-      <nav>
-        <Link to = "/computer">Continue</Link>
-      </nav>
+    <div className="Home-container">
+      <p>Bla Bla Bla Something something. Bla Bla</p>
     </div>
   );
 };
